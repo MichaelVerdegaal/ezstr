951ac58b8affbeac36c5dcc8df229807d6a17ad3
Michael V <michael.verdegaal@hva.nl>
2019-04-11 13:46:18 +0200

    Repair file structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ezstr',
+      version='0.1',
+      description='No more tostring boilerplate',
+      url='https://github.com/MichaelVerdegaal/ezstr',
+      author='Michael Verdegaal',
+      author_email='michaelverdegaal@hotmail.nl',
+      license='MIT',
+      packages=['ezstr'],
+      zip_safe=False)
