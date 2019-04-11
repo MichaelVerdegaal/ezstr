c535cfc4ac01b4dec9cc1f3f67afe5341b8341f8
Michael V <michael.verdegaal@hva.nl>
2019-04-11 14:24:53 +0200

    Fix readme not being readable for pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='ezstr',
       version='0.1',
       description='No more tostring boilerplate',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/MichaelVerdegaal/ezstr',
       author='Michael Verdegaal',
       author_email='michaelverdegaal@hotmail.nl',
